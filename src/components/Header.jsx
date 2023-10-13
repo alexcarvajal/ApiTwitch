f8b6cef1e6e569bc684938bb56100fb14340c1df
alexcarvajal <alexcarvajal@unicauca.edu.co>
2023-10-13 10:24:04 -0500

    Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Header = ({ title }) => {
     return (
         <div >
-            <div className={`flex flex-row bg-custom-Header h-24 text-white `}>
-               <Link to='/' className='mt-4'> <img src={TwitchLogo} className='w-16 sticky' alt="" /></Link>
+            <div className={`flex flex-row bg-custom-Header h-24 relative text-white `}>
+               <Link to='/' className='mt-4'> <img src={TwitchLogo} className='w-16 fixed' alt="" /></Link>
                 <h1 className="flex text-3xl font-bold items-center  mx-auto ">{title}</h1>
             </div>
         </div>
